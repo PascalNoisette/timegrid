746f505665d6fa8c83ad78d2fa8d6c3170e46f1e
Pascal Noisette <netpascal0123@aol.com>
2020-07-10 07:49:46 +0200

    Use www-data as app user

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,8 @@ FROM debian:8
 
 MAINTAINER timegrid@pega.sh
 
-ARG user=user
-ARG uid=1000
-
 RUN apt-get update && \
     apt-get install apt-utils -y
-
-RUN adduser --disabled-password --gecos "" -u $uid $user
 
 RUN apt-get install php5 php5-mysql git wget curl php5-curl php5-intl phpunit vim -y 
 
@@ -19,9 +14,7 @@ WORKDIR /var/www
 RUN mkdir timegrid && chown $user:www-data timegrid && \
     echo "* * * * * php /var/www/timegrid/artisan schedule:run >> /dev/null 2>&1" >> /etc/crontab
 
-USER $user
-
-COPY --chown=$user . timegrid
+COPY --chown=www-data . timegrid
 
 WORKDIR /var/www/timegrid
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+echo "Updating www-data uid and gid"
+
+DOCKER_UID=`stat -c "%u" server.php`
+DOCKER_GID=`stat -c "%g" server.php`
+
+INCUMBENT_USER=`getent passwd $DOCKER_UID | cut -d: -f1`
+INCUMBENT_GROUP=`getent group $DOCKER_GID | cut -d: -f1`
+
+echo "Docker: uid = $DOCKER_UID, gid = $DOCKER_GID"
+echo "Incumbent: user = $INCUMBENT_USER, group = $INCUMBENT_GROUP"
+
+# Once we've established the ids and incumbent ids then we need to free them
+# up (if necessary) and then make the change to www-data.
+
+[ ! -z "${INCUMBENT_USER}" ] && usermod -u 99$DOCKER_UID $INCUMBENT_USER
+usermod -u $DOCKER_UID www-data
+
+[ ! -z "${INCUMBENT_GROUP}" ] && groupmod -g 99$DOCKER_GID $INCUMBENT_GROUP
+groupmod -g $DOCKER_GID www-data
+
 echo "Checking database"
 while [[ `echo "DB::connection()->getPdo();" | php artisan tinker` == *"SQLSTATE"* ]]
 do
